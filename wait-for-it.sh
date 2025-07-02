e4d6c34d85dcb8b570ac15a7a154ab7e32e8cbb6
Iuri <iuriguilhermesantos@gmail.com>
2025-07-02 16:56:41 -0300

    aaa

---
+++ wait-for-it.sh
@@ -0,0 +1,16 @@
+set -e
+
+host="$1"
+shift
+port="$1"
+shift
+
+cmd="$@"
+
+until nc -z "$host" "$port"; do
+  >&2 echo "Aguardando $host:$port..."
+  sleep 1
+done
+
+>&2 echo "$host:$port está pronto — iniciando aplicação"
+exec $cmd
